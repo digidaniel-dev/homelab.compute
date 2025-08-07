326e9873bc33b4de287a1b8a8c832a8cf87bb42c
Daniel Vollbro <wollbro90@gmail.com>
2025-08-07 20:10:47 +0200

    Removed unused variables

--- gryffindor/variables.tf
+++ gryffindor/variables.tf
@@ -4,46 +4,13 @@ variable "endpoint" {
   type = string
 }
 
-// Proxmox ACME Plugin
-
-variable "cloudflare_account_id" {
-  type      = string
-  sensitive = true
-}
-
-variable "cloudflare_api_token" {
-  type      = string
-  sensitive = true
-}
-
-// Proxmox ACME Account
-
-variable "email" {
-  type = string
-}
-
-// SSH provider
-
-variable "password" {
-  type      = string
-  sensitive = true
-}
-
-variable "ssh_user" {
-  type = string
-}
-
-variable "ssh_password" {
-  type      = string
-  sensitive = true
-}
-
 // API Provider
 
 variable "api_token" {
   type      = string
   sensitive = true
 }
+
 // Generic VM
 
 variable "ssh_key_path" {
